feb252cf54c106c211ba16b61d02a0d92eae58e2
yab94 <yab94>
2026-03-26 21:26:30 +0100

    fix

--- src/Framework/Typable.php
+++ src/Framework/Typable.php
@@ -8,46 +8,57 @@ use InvalidArgumentException;
 
 class Typable
 {
-    public function __construct(protected array $data)
+    private static object $MISSING;
+
+    public function __construct(protected array $data) {}
+
+    public static function missing(): object
     {
+        return self::$MISSING ??= new \stdClass();
     }
 
     public function get(string $key, mixed $default = null): mixed
     {
-        return $this->data[$key] ?? $default;
-    }
-
-    final public function getArray(string $path, array $default = []): array
-    {
-        $value = $this->get($path, $default);
-        if (!is_array($value)) {
-            throw new \InvalidArgumentException("Param '$path' is not an array");
+        $value = $this->data[$key] ?? self::missing();
+        if ($value === self::$MISSING) {
+            if (func_num_args() < 2) {
+                throw new \InvalidArgumentException("Param '$key' is required.");
+            }
+            return $default;
         }
-
         return $value;
     }
 
-    final public function getInt(string $key, int $default = 0): int
+    final public function getArray(string $path, ?array $default = null): array
     {
-        $value = $this->get($key, $default);
+        $value = $default !== null ? $this->get($path, $default) : $this->get($path);
+        if (!is_array($value)) {
+            throw new \InvalidArgumentException("Param '$path' is not an array");
+        }
+        return $value;
+    }
+
+    final public function getInt(string $key, ?int $default = null): int
+    {
+        $value = $default !== null ? $this->get($key, $default) : $this->get($key);
         if (!is_numeric($value)) {
             throw new \InvalidArgumentException("Param '$key' is not numeric");
         }
-        return (int)$value;
+        return (int) $value;
     }
 
-    final public function getFloat(string $key, float $default = 0.0): float
+    final public function getFloat(string $key, ?float $default = null): float
     {
-        $value = $this->get($key, $default);
+        $value = $default !== null ? $this->get($key, $default) : $this->get($key);
         if (!is_numeric($value)) {
             throw new \InvalidArgumentException("Param '$key' is not numeric");
         }
-        return (float)$value;
+        return (float) $value;
     }
 
-    final public function getString(string $key, string $default = ''): string
+    final public function getString(string $key, ?string $default = null): string
     {
-        $value = $this->get($key, $default);
+        $value = $default !== null ? $this->get($key, $default) : $this->get($key);
         if (!is_string($value)) {
             throw new \InvalidArgumentException("Param '$key' is not a string");
         }
