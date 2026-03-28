278b7083c37c68b2a07f84ad54d5c45c5b34e0e9
yab94 <yab94>
2026-03-28 10:50:34 +0100

    f

--- src/Infrastructure/Mail/SmtpMailerAdapter.php
+++ src/Infrastructure/Mail/SmtpMailerAdapter.php
@@ -4,35 +4,29 @@ declare(strict_types=1);
 
 namespace Rore\Infrastructure\Mail;
 
-use RRB\Bootstrap\Config;
-use RRB\Di\Bind;
+use RRB\Di\BindConfig;
 use RRB\Mail\SmtpMailer;
 use Rore\Application\Contact\Port\MailerInterface;
 
-final class SmtpMailerAdapter implements MailerInterface
+final class SmtpMailerAdapter extends SmtpMailer implements MailerInterface
 {
     public function __construct(
-        #[Bind(static function (Config $c): SmtpMailer {
-            return new SmtpMailer(
-                host:       $c->getString('smtp.host'),
-                port:       $c->getInt('smtp.port', 587),
-                encryption: strtolower($c->getString('smtp.encryption', 'tls')),
-                user:       $c->getString('smtp.user'),
-                password:   $c->getString('smtp.password'),
-                fromEmail:  $c->getString('smtp.from_email'),
-                fromName:   $c->getString('smtp.from_name', $c->getString('smtp.from_email')),
-            );
-        })]
-        private readonly SmtpMailer $mailer,
-    ) {}
-
-    public function send(
-        string  $to,
-        string  $subject,
-        string  $body,
-        ?string $replyTo = null,
-        bool    $isHtml  = false,
-    ): void {
-        $this->mailer->send($to, $subject, $body, $replyTo, $isHtml);
+        #[BindConfig('smtp.host')]
+        string $host,
+        #[BindConfig('smtp.port')]
+        int    $port       = 587,
+        #[BindConfig('smtp.encryption')]
+        string $encryption = 'tls',
+        #[BindConfig('smtp.user')]
+        string $user       = '',
+        #[BindConfig('smtp.password')]
+        string $password   = '',
+        #[BindConfig('smtp.from_email')]
+        string $fromEmail  = '',
+        #[BindConfig('smtp.from_name')]
+        string $fromName   = '',
+    ) {
+        parent::__construct($host, $port, strtolower($encryption), $user, $password, $fromEmail, $fromName ?: $fromEmail);
     }
 }
+
